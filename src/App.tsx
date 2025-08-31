c60a2e06427cfce97608838c2a873edae7ffcf74
Maximilian <scientificresearch309@gmail.com>
2025-08-31 18:54:46 +0200

    the title and author are there

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import React from 'react';
 import './App.css';
 import { books } from './dev/books';
 
 function App() {
-  books.map((b) => console.log(b));
-
   return (
     <div className="App">
       <h1>Nested Components in React</h1>
+      <div className="books">
+        {books.map((b) => (
+          <React.Fragment key={b.id}>
+            <div className="book">
+              <p className="title">
+                <span className="title">Title:</span> {b.title}
+              </p>
+              <p className="author">
+                <span className="author">Author:</span> {b.author}
+              </p>
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
