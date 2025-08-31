0899db9a26abef79f5aff48296501681ed41962f
Maximilian <scientificresearch309@gmail.com>
2025-08-31 18:40:13 +0200

    the books are there in books.tsx and displaying the list of the books in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css';
+import { books } from './dev/books';
 
 function App() {
+  books.map((b) => console.log(b));
+
   return (
     <div className="App">
       <h1>Nested Components in React</h1>
